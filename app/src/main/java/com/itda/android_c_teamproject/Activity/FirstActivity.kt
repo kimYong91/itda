4eee9d8bcd73482595336b165fe2b081c1d9e143
KimYong91 <rladyd97@naver.com>
2024-06-26 12:02:03 +0900

    운동 계획 목록 저장 수정중

--- app/src/main/java/com/itda/android_c_teamproject/Activity/FirstActivity.kt
+++ app/src/main/java/com/itda/android_c_teamproject/Activity/FirstActivity.kt
@@ -48,7 +48,7 @@ class FirstActivity : AppCompatActivity() {
         }
 
         // 메인화면 좌측 상단 사용자 정보 선택란
-        val items = arrayOf("정보", "개인정보", "건강정보", "로그아웃")
+        val items = arrayOf("정보", "개인정보", "건강정보", "운동 계획", "로그아웃")
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
         binding.spinner.adapter = adapter
@@ -80,6 +80,15 @@ class FirstActivity : AppCompatActivity() {
                     }
 
                     3 -> {
+                        startActivity(
+                            Intent(
+                                this@FirstActivity,
+                                MemoActivity::class.java
+                            )
+                        )
+                    }
+
+                    4 -> {
                         logout()
                     }
                 }
